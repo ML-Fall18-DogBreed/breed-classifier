6893ce302e5dab354d49b409e65f518932eda701
dpwivagg <dpwivagg@wpi.edu>
2018-11-22 11:39:56 -0500

    Add model weights from Ace training

--- cnn_classifier/cnn-job.sh
+++ cnn_classifier/cnn-job.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 #SBATCH --output=simple.out #SBATCH --error=simple.err
-#SBATCH --gres:gpu:1
+#SBATCH --gres=gpu:2
 #SBATCH -t 0:45:00
 
 module load cudnn
 module load cuda90/toolkit
 module load cuda90/blas
+module load cuda90/profiler
+module load cuda90/nsight
+module load cuda90/fft
 source ~/venv/bin/activate
 python classifier.py
